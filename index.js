182d3d41644a51ba2713620421c7e1360a1b1cfc
Abdul haseeb <haseebmangal@yahoo.com>
2024-07-28 13:31:25 +0500

    javaScript problem solved

---
+++ index.js
@@ -0,0 +1,31 @@
+// problem 01
+// Given the string "123abc", use parseInt to convert it to a number and explain the result
+// var string1 = "123abc";
+// console.log(typeof string1);
+// var converted = parseInt(string1)
+// console.log(converted);
+
+// problem 02
+// Given the string "45.67xyz" , use parseFloat to convert it ot a number and explain the result
+// var string2 = "45.67xyz";
+// var convert = parseFloat(string2);
+// console.log(typeof convert);
+
+// problem 03
+// Given the string "123.45", use the number function to converyt it to a number and explain the result.
+// var string3 = "123.45";
+// var convert = parseInt(string3);
+// console.log(typeof convert + convert);
+
+// problem 04
+// given the number 789, use the toString method to convert it to a string.
+// var num = 789;
+// console.log(typeof num)
+// var str = num.toString();
+// console.log(typeof str)
+
+// problem 05
+var str = "256px";
+console.log("before " + typeof str + str);
+var convert = parseInt(str);
+console.log("after " + typeof convert + convert)
